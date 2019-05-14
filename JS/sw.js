55a3cd2495c9b94d4d1efac13375e787cbc03215
SirAnvar <liammanscott@gmail.com>
2019-05-14 16:18:52 +0100

    added service worker

---
+++ JS/sw.js
@@ -0,0 +1,33 @@
+
+self.addEventListener('fetch', function(event) {
+event.respondWith(caches.open('cache').then(function(cache) {
+return cache.match(event.request).then(function(response) {
+console.log("cache request: " + event.request.url);
+var fetchPromise = fetch(event.request).then(function(networkResponse) { 
+// if we got a response from the cache, update the cache 
+console.log("fetch completed: " + event.request.url, networkResponse);
+if (networkResponse) {
+ console.debug("updated cached page: " + event.request.url, networkResponse);
+   cache.put(event.request, networkResponse.clone());}
+   return networkResponse;},
+// rejected promise - just ignore it, we're offline! 
+     console.log("Error in fetch()", event));
+     event.waitUntil(caches.open('cache').then(function(cache) {
+ // our cache is named *cache* in the caches.open() above
+     return cache.addAll ([ 
+      '/', // do not remove this
+      '/index.html', // cache index page
+      '/images/*',// choose images to keep offline, just an example
+      ]);
+   })
+  );
+});
+ // respond from the cache, or the network
+return response || fetchPromise;
+}));
+});
+
+self.addEventListener('install', function(event) {
+   self.skipWaiting();
+   console.log("Latest version installed!");
+});
